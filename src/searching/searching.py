7aee124d1006b4c6aee8e81b38e92c2fbf203e4c
Eric SarragaLugo <ericlugo@users.noreply.github.com>
2020-08-06 20:56:07 -0400

    cleaned up searching code

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -9,13 +9,12 @@ def binary_search(arr, target):
     if 0 < len(arr):
         start_pointer = 0
         end_pointer = len(arr) - 1
-        target_pointer = end_pointer // 2
-        while (start_pointer != target_pointer) and (end_pointer != target_pointer):
-            if arr[target_pointer] == target:
-                return target_pointer
-            elif target < arr[target_pointer]:
+        while start_pointer <= end_pointer:
+            target_pointer = (start_pointer + end_pointer) // 2
+            if target < arr[target_pointer]:
                 end_pointer = target_pointer
-            else:
+            elif arr[target_pointer] < target:
                 start_pointer = target_pointer
-            target_pointer = start_pointer + ((end_pointer-start_pointer) // 2)
+            else:
+                return target_pointer
     return -1
