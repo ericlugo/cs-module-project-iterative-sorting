073e47d5870188ad49cabf199b1d4b74dd6c9f67
Eric SarragaLugo <ericlugo@users.noreply.github.com>
2020-08-06 20:48:00 -0400

    searching complete

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,21 @@
 def linear_search(arr, target):
-    # Your code here
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1
 
 
-    return -1   # not found
-
-
-# Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
-
-
-    return -1  # not found
+    if 0 < len(arr):
+        start_pointer = 0
+        end_pointer = len(arr) - 1
+        target_pointer = end_pointer // 2
+        while (start_pointer != target_pointer) and (end_pointer != target_pointer):
+            if arr[target_pointer] == target:
+                return target_pointer
+            elif target < arr[target_pointer]:
+                end_pointer = target_pointer
+            else:
+                start_pointer = target_pointer
+            target_pointer = start_pointer + ((end_pointer-start_pointer) // 2)
+    return -1
